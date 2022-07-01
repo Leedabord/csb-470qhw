3cb750dc5739ad645dbe3e195054f917668871e1
Code Sandbox <hello@codesandbox.io>
2022-07-01 16:02:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8b28f8e34829393aa79a671e627f164c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24aca7570bfed7be5ae0",
+    "url": "https://leedabord.github.io/csb-470qhw/static/css/main.54c6ff04.chunk.css"
+  },
+  {
+    "revision": "24aca7570bfed7be5ae0",
+    "url": "https://leedabord.github.io/csb-470qhw/static/js/main.865ebef1.chunk.js"
+  },
+  {
+    "revision": "5a5e37a58bbcc7b88c70",
+    "url": "https://leedabord.github.io/csb-470qhw/static/js/runtime~main.96609cb3.js"
+  },
+  {
+    "revision": "3570ff6d80399f2376a2",
+    "url": "https://leedabord.github.io/csb-470qhw/static/css/2.9e1a1efb.chunk.css"
+  },
+  {
+    "revision": "3570ff6d80399f2376a2",
+    "url": "https://leedabord.github.io/csb-470qhw/static/js/2.ce950991.chunk.js"
+  },
+  {
+    "revision": "e12c9166d763a6ca0bf2d1d5ad083dfb",
+    "url": "https://leedabord.github.io/csb-470qhw/index.html"
+  }
+];
